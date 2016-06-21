63291f1b26a137bbccec88ad7a9b0b7c92a99d0b
michelsylv <sylvainmichel@live.fr>
2016-06-21 06:22:40 +0200

    add field for recipe entity

--- web/modules/custom/recipes/src/Entity/RecipeEntity.php
+++ web/modules/custom/recipes/src/Entity/RecipeEntity.php
@@ -36,6 +36,7 @@ use Drupal\user\UserInterface;
  *   },
  *   base_table = "recipe",
  *   admin_permission = "administer recipe entity entities",
+ *   translatable = TRUE,
  *   entity_keys = {
  *     "id" = "id",
  *     "label" = "name",
@@ -54,179 +55,210 @@ use Drupal\user\UserInterface;
  *   field_ui_base_route = "recipe.settings"
  * )
  */
-class RecipeEntity extends ContentEntityBase implements RecipeEntityInterface {
+class RecipeEntity extends ContentEntityBase implements RecipeEntityInterface
+{
 
-  use EntityChangedTrait;
+    use EntityChangedTrait;
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
-    parent::preCreate($storage_controller, $values);
-    $values += array(
-      'user_id' => \Drupal::currentUser()->id(),
-    );
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public static function preCreate(EntityStorageInterface $storage_controller, array &$values)
+    {
+        parent::preCreate($storage_controller, $values);
+        $values += array(
+            'user_id' => \Drupal::currentUser()->id(),
+        );
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getName() {
-    return $this->get('name')->value;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return $this->get('description')->value;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setName($name) {
-    $this->set('name', $name);
-    return $this;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function setDescription($description)
+    {
+        $this->set('description', $description);
+        return $this;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getCreatedTime() {
-    return $this->get('created')->value;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->get('name')->value;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setCreatedTime($timestamp) {
-    $this->set('created', $timestamp);
-    return $this;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function setName($name)
+    {
+        $this->set('name', $name);
+        return $this;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwner() {
-    return $this->get('user_id')->entity;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedTime()
+    {
+        return $this->get('created')->value;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwnerId() {
-    return $this->get('user_id')->target_id;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatedTime($timestamp)
+    {
+        $this->set('created', $timestamp);
+        return $this;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwnerId($uid) {
-    $this->set('user_id', $uid);
-    return $this;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwner()
+    {
+        return $this->get('user_id')->entity;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwner(UserInterface $account) {
-    $this->set('user_id', $account->id());
-    return $this;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwnerId()
+    {
+        return $this->get('user_id')->target_id;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function isPublished() {
-    return (bool) $this->getEntityKey('status');
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwnerId($uid)
+    {
+        $this->set('user_id', $uid);
+        return $this;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setPublished($published) {
-    $this->set('status', $published ? NODE_PUBLISHED : NODE_NOT_PUBLISHED);
-    return $this;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwner(UserInterface $account)
+    {
+        $this->set('user_id', $account->id());
+        return $this;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
-    $fields['id'] = BaseFieldDefinition::create('integer')
-      ->setLabel(t('ID'))
-      ->setDescription(t('The ID of the Recipe.'))
-      ->setReadOnly(TRUE);
-    $fields['uuid'] = BaseFieldDefinition::create('uuid')
-      ->setLabel(t('UUID'))
-      ->setDescription(t('The UUID of the Recipe.'))
-      ->setReadOnly(TRUE);
+    /**
+     * {@inheritdoc}
+     */
+    public function isPublished()
+    {
+        return (bool)$this->getEntityKey('status');
+    }
 
-    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('Authored by'))
-      ->setDescription(t('The user ID of author of the Recipe.'))
-      ->setRevisionable(TRUE)
-      ->setSetting('target_type', 'user')
-      ->setSetting('handler', 'default')
-      ->setDefaultValueCallback('Drupal\node\Entity\Node::getCurrentUserId')
-      ->setTranslatable(TRUE)
-      ->setDisplayOptions('view', array(
-        'label' => 'hidden',
-        'type' => 'author',
-        'weight' => 0,
-      ))
-      ->setDisplayOptions('form', array(
-        'type' => 'entity_reference_autocomplete',
-        'weight' => 5,
-        'settings' => array(
-          'match_operator' => 'CONTAINS',
-          'size' => '60',
-          'autocomplete_type' => 'tags',
-          'placeholder' => '',
-        ),
-      ))
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE);
+    /**
+     * {@inheritdoc}
+     */
+    public function setPublished($published)
+    {
+        $this->set('status', $published ? NODE_PUBLISHED : NODE_NOT_PUBLISHED);
+        return $this;
+    }
 
-    $fields['name'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('Name'))
-      ->setDescription(t('The name of the Recipe.'))
-      ->setSettings(array(
-        'max_length' => 50,
-        'text_processing' => 0,
-      ))
-      ->setDefaultValue('')
-      ->setDisplayOptions('view', array(
-        'label' => 'above',
-        'type' => 'string',
-        'weight' => -4,
-      ))
-      ->setDisplayOptions('form', array(
-        'type' => 'string_textfield',
-        'weight' => -4,
-      ))
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE);
+    /**
+     * {@inheritdoc}
+     */
+    public static function baseFieldDefinitions(EntityTypeInterface $entity_type)
+    {
+        $fields['id'] = BaseFieldDefinition::create('integer')
+            ->setLabel(t('ID'))
+            ->setDescription(t('The ID of the Recipe.'))
+            ->setReadOnly(TRUE);
+        $fields['uuid'] = BaseFieldDefinition::create('uuid')
+            ->setLabel(t('UUID'))
+            ->setDescription(t('The UUID of the Recipe.'))
+            ->setReadOnly(TRUE);
 
-    $fields['status'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Publishing status'))
-      ->setDescription(t('A boolean indicating whether the Recipe entity is published.'))
-      ->setDefaultValue(TRUE)
-        ->setDisplayConfigurable('form', TRUE);
+        $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
+            ->setLabel(t('Authored by'))
+            ->setDescription(t('The user ID of author of the Recipe.'))
+            ->setRevisionable(TRUE)
+            ->setSetting('target_type', 'user')
+            ->setSetting('handler', 'default')
+            ->setDefaultValueCallback('Drupal\node\Entity\Node::getCurrentUserId')
+            ->setTranslatable(TRUE)
+            ->setDisplayOptions('view', array(
+                'label' => 'hidden',
+                'type' => 'author',
+                'weight' => 0,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'entity_reference_autocomplete',
+                'weight' => 5,
+                'settings' => array(
+                    'match_operator' => 'CONTAINS',
+                    'size' => '60',
+                    'autocomplete_type' => 'tags',
+                    'placeholder' => '',
+                ),
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['name'] = BaseFieldDefinition::create('string')
+            ->setLabel(t('Name'))
+            ->setTranslatable(TRUE)
+            ->setDescription(t('The name of the Recipe.'))
+            ->setSettings(array(
+                'max_length' => 50,
+                'text_processing' => 0,
+            ))
+            ->setDefaultValue('')
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => -4,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'string_textfield',
+                'weight' => -4,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['status'] = BaseFieldDefinition::create('boolean')
+            ->setLabel(t('Publishing status'))
+            ->setDescription(t('A boolean indicating whether the Recipe entity is published.'))
+            ->setDefaultValue(TRUE)
+            ->setDisplayConfigurable('form', TRUE);
 
 
-    $fields['langcode'] = BaseFieldDefinition::create('language')
-      ->setLabel(t('Language code'))
-      ->setDescription(t('The language code for the Recipe.'))
-      ->setDisplayOptions('form', array(
-        'type' => 'language_select',
-        'weight' => 10,
-      ))
-      ->setDisplayConfigurable('form', TRUE);
+        $fields['langcode'] = BaseFieldDefinition::create('language')
+            ->setLabel(t('Language code'))
+            ->setDescription(t('The language code for the Recipe.'))
+            ->setDisplayOptions('form', array(
+                'type' => 'language_select',
+                'weight' => 10,
+            ))
+            ->setDisplayConfigurable('form', TRUE);
 
-    $fields['created'] = BaseFieldDefinition::create('created')
-      ->setLabel(t('Created'))
-      ->setDescription(t('The time that the entity was created.'));
+        $fields['created'] = BaseFieldDefinition::create('created')
+            ->setLabel(t('Created'))
+            ->setDescription(t('The time that the entity was created.'));
 
-    $fields['changed'] = BaseFieldDefinition::create('changed')
-      ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the entity was last edited.'));
+        $fields['changed'] = BaseFieldDefinition::create('changed')
+            ->setLabel(t('Changed'))
+            ->setDescription(t('The time that the entity was last edited.'));
 
-    return $fields;
-  }
+        return $fields;
+    }
 
 }
